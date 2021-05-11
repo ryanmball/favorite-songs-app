cd83fd14129166569e27a9a61e07ec1863501e99
ryan <ball.ryan.rb@gmail.com>
2021-05-11 12:27:03 -0600

    adds RESTful routes and controller methods

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,38 @@
 class SongsController < ApplicationController
+  def index
+    render json: Song.all.as_json
+  end
+
+  def show
+    render json: Song.find(params[:id]).as_json
+  end
+
+  def create
+    song = Song.new(
+      title: params[:title],
+      album: params[:album],
+      artist: params[:artist],
+      year: params[:year],
+      link: params[:link]
+    )
+    song.save
+    render json: song.as_json
+  end
+
+  def update
+    song = Song.find(params[:id])
+    song.title = params[:title] || song.title
+    song.album = params[:album] || song.album
+    song.artist = params[:artist] || song.artist
+    song.year = params[:year] || song.year
+    song.link = params[:link] || song.link
+    song.save
+    render json: song.as_json
+  end
+
+  def destroy
+    song = Song.find(params[:id])
+    song.delete
+    render json: {message: "Song successfully deleted"}
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Song.create(title: "Not Now", album: "Greatest Hits", artist: "blink-182", year: 2003, link: "https://open.spotify.com/track/28yCMEplHbodvR19Aipah8?si=d335e8caae254654")
+
+Song.create(title: "Notorious", album: "Notorious", artist: "Malaa", year: 2016, link: "https://open.spotify.com/track/74cvkvTVxAXCew97vBnYiy?si=0d9e61a08aed4b09")
+
+Song.create(title: "Forever - Garage Version", album: "Delta", artist: "Mumford & Sons", year: 2020, link: "https://open.spotify.com/track/4gSwwP1tCXtTFemHjaVYOc?si=ca0cdbfea4f9466a")
