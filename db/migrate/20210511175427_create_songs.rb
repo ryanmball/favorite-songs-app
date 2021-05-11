c7e50fef202c3e1d620205480f4053cf80c01aa7
ryan <ball.ryan.rb@gmail.com>
2021-05-11 11:55:28 -0600

    initial commit

---
+++ db/migrate/20210511175427_create_songs.rb
@@ -0,0 +1,13 @@
+class CreateSongs < ActiveRecord::Migration[6.1]
+  def change
+    create_table :songs do |t|
+      t.string :title
+      t.string :album
+      t.string :artist
+      t.integer :year
+      t.string :link
+
+      t.timestamps
+    end
+  end
+end
